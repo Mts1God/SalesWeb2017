2485c317b7b22cc4f1b0ea4ccdbf852c6316e70e
Mateus Felipe Ribeiro <nogueira.mateus20k1@outlook.com>
2024-05-17 11:57:07 -0300

    Seeding Service

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
 {
@@ -13,14 +12,19 @@ namespace SalesWebMvc.Models
 
         public Department()
         {
-
         }
 
-        public Department(int id, string name, ICollection<Seller> sellers)
+        public Department(int id, string name)
         {
             Id = id;
             Name = name;
         }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
         public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
